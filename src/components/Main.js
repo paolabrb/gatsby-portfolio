65712c8f4bbbc80a6507fb4fb3aa4a1810ebc1b6
Paola Barboglio <paola.barboglio@gmail.com>
2020-04-27 18:24:34 +0200

    update icon links in contact and hover css

--- src/components/Main.js
+++ src/components/Main.js
@@ -177,29 +177,18 @@ class Main extends React.Component {
           </form>
           <ul className="icons">
             <li>
-              <a
-                href="https://twitter.com/HuntaroSan"
-                className="icon fa-twitter"
-              >
-                <span className="label">Twitter</span>
+              <a href="https://github.com/paolabrb" className="icon fa-github" target="_blank">
+                <span className="label">Github</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-facebook">
-                <span className="label">Facebook</span>
+              <a href="https://linkedin.com/in/paolabarboglio" className="icon fa-linkedin" target="_blank">
+                <span className="label">LinkedIN</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-instagram">
+              <a href="https://www.instagram.com/paolabrbg/" className="icon fa-instagram" target="_blank">
                 <span className="label">Instagram</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/codebushi/gatsby-starter-dimension"
-                className="icon fa-github"
-              >
-                <span className="label">GitHub</span>
               </a>
             </li>
           </ul>
