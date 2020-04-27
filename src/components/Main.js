7b263305ca0eb757b6f0705d254445f47499a386
Paola Barboglio <paola.barboglio@gmail.com>
2020-04-27 15:52:44 +0200

    install bootstrap

--- src/components/Main.js
+++ src/components/Main.js
@@ -56,24 +56,21 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Work</h2>
-          <span className="image main">
+          {/* <span className="image main">
             <img src={pic02} alt="" />
-          </span>
-          <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
-          </p>
-          <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
-            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-            tempus.
-          </p>
+          </span> */}
+          <div class="accordion" id="faq-accordion">
+
+<h4 id="question-1" class="collapsed" data-toggle="collapse" data-target="#faq1" aria-expanded="false" aria-controls="faq1">How did you learn to code?</h4>
+<p id="faq1" class="collapse" aria-labelledby="question-1" data-parent="#faq-accordion">I was always interested in programming, but didn't know where to start. I attended Le Wagon's full stack bootcamp in October 2019 in Berlin - that's how my coding journey began!</p>
+
+<h4 id="question-2" class="collapsed" data-toggle="collapse" data-target="#faq2" aria-expanded="false" aria-controls="faq2">A bootcamp student? How much programming can you learn in just some weeks?</h4>
+<p id="faq2" class="collapse" aria-labelledby="question-2" data-parent="#faq-accordion">I learned <strong>a lot</strong> during the bootcamp. I went from knowing basic css to building functioning apps with Rails in just nine weeks. It was just the beginning - I'm ready to learn more!</p>
+<h4 id="question-3" class="collapsed" data-toggle="collapse" data-target="#faq3" aria-expanded="false" aria-controls="faq3">Alright - so you live in Germany. Can you speak German?</h4>
+<p id="faq3" class="collapse" aria-labelledby="question-3" data-parent="#faq-accordion">My academic background is actually a linguistic one - I have studied German and Russian. I speak fluent German and some other languages, too! More on this in my <strong>CV</strong> 👇</p>
+<h4 id="question-4" class="collapsed" data-toggle="collapse" data-target="#faq4" aria-expanded="false" aria-controls="faq4">Apart from code mastery - do you have any further skills?</h4>
+<p id="faq4" class="collapse" aria-labelledby="question-4" data-parent="#faq-accordion">In a past professional life I was an Executive Assistant. I am a structured, organized and curious individual. I <strong>will</strong> deliver great results, be it under pressure or time constraints.</p>
+</div>
           {close}
         </article>
 
