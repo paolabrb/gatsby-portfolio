1be68c5f34d904990cfab6cdac2d286d60c5da4a
Paola Barboglio <paola.barboglio@gmail.com>
2020-04-28 16:16:05 +0200

    set up and customize 404 and 500 pages

--- src/pages/500.js
+++ src/pages/500.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Layout from '../components/layout'
 import { Link } from 'gatsby'
 
-const NotFoundPage = () => (
+const ServerErrorPage = () => (
   <Layout>
-    <h1>// 404 page not found</h1>
+    <h1>// 500 internal server error</h1>
     <p>Would you like to go <Link to="/">back</Link>?</p>
   </Layout>
 )
 
-export default NotFoundPage
+export default ServerErrorPage
